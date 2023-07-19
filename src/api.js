f0c2249be7b54ce859570a1fbad33ad4879152ac
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-07-19 16:14:13 +0300

    update index.js/add pagination

--- src/api.js
+++ src/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
-async function serviceImagesByInputValue(value) {
+async function serviceImagesByInputValue(value, page) {
   const params = new URLSearchParams({
     key: '38324496-84226fc1fa5e9d21681883b31',
     q: value,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: 40,
   });
   const { data } = await axios(`?${params}`);
   return data;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,47 @@ import Notiflix from 'notiflix';
 import 'notiflix/src/notiflix.css';
 import { formToJSON } from 'axios';
 
-const searchForm = document.querySelector('.search-form');
-const galleryContainer = document.querySelector('.gallery');
+selectors = {
+  searchForm: document.querySelector('.search-form'),
+  galleryContainer: document.querySelector('.gallery'),
+  input: document.querySelector('input'),
+  guard: document.querySelector('.js-guard'),
+};
 
-searchForm.addEventListener('submit', onSearch);
+selectors.searchForm.addEventListener('submit', onSearch);
 
-async function onSearch(evt) {
+selectors.input.addEventListener('input', clearGallery);
+
+const options = {
+  root: null,
+  rootMargin: '200px',
+  threshold: 0,
+};
+
+let page = 1;
+
+const observer = new IntersectionObserver(onPagination, options);
+
+function onPagination(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      serviceImagesByInputValue(value, page).then(data => {
+        const { hits, total } = data;
+
+        selectors.galleryContainer.insertAdjacentHTML(
+          'beforeend',
+          createImageMarkup({ hits })
+        );
+        if (page >= total) {
+          observer.unobserve(selectors.guard);
+        }
+      });
+    }
+  });
+}
+
+function onSearch(evt) {
   evt.preventDefault();
 
   const { searchQuery } = evt.currentTarget.elements;
@@ -18,34 +53,43 @@ async function onSearch(evt) {
     return Notiflix.Notify.failure('Search field cannot be empty!');
   }
 
-  try {
-    const { hits } = await serviceImagesByInputValue(inputValue);
+  serviceImagesByInputValue(inputValue, page)
+    .then(data => {
+      const { hits, total } = data;
 
-    // if (!{ hits }.length) {
-    //   throw new Error(
-    //     'Sorry, there are no images matching your search query. Please try again.'
-    //   );
-    // }
+      if (!hits.length) {
+        throw new Error(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
 
-    galleryContainer.innerHTML = createImageMarkup({ hits });
-  } catch (error) {
-    handleError(error);
-  }
+      selectors.galleryContainer.insertAdjacentHTML(
+        'beforeend',
+        createImageMarkup({ hits })
+      );
+
+      if (page < total) {
+        observer.observe(selectors.guard);
+      }
+    })
+    .catch(error => handleError(error))
+    .finally(evt.currentTarget.reset());
 }
 
 function createImageMarkup({ hits }) {
-  return hits.map(
-    ({
-      webformatURL,
-      largeImageURL,
-      tags,
-      likes,
-      views,
-      comments,
-      downloads,
-    }) => {
-      return `<div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" width="300" />
   <div class="info">
     <p class="info-item">
       <b>Likes</b>
@@ -65,10 +109,15 @@ function createImageMarkup({ hits }) {
     </p>
   </div>
 </div>`;
-    }
-  );
+      }
+    )
+    .join('');
 }
 
 function handleError(error) {
   Notiflix.Notify.failure(error.message);
+}
+
+function clearGallery() {
+  selectors.galleryContainer.innerHTML = '';
 }
